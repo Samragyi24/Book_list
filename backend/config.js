214bc93c4c6e3941269561a896f9aff8d139ca9a
Samragyi Sharma <83724634+Samragyi24@users.noreply.github.com>
2024-03-15 23:43:58 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://samragiisharma:gyaarsi24@cluster0.sx83abj.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Cluster0'
---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoutes.js';
+import cors from 'cors';
+
+const app = express(); 
+
+app.use(express.json());
+
+app.use(cors());
+
+
+
+app.get('/',(request,response)=>{
+ console.log(request)
+ return response.status(234).send('Welcome');
+});
+
+app.use('/books',booksRoute);
+
+mongoose
+ .connect(mongoDBURL)
+ .then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, ()=>{
+    console.log(`App is listenening to port: ${PORT}`)
+   });
+ })
+ .catch((error)=>{
+    console.log(error);
+ })
